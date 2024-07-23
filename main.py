36ed9ea992a05dd58e80936060069c14b28d86c7
Yabbanootje <k.a.b.toonen@student.tue.nl>
2024-07-23 11:39:08 +0200

    Try to finish ppoet

--- main.py
+++ main.py
@@ -195,10 +195,10 @@ if __name__ == '__main__':
     # baseline_algorithms = []#["PPO", "CPO", "OnCRPO", "CUP", "FOCOPS", "PCPO", "PPOEarlyTerminated", "PPOLag"]
     # curr_algorithms = ["PPOLag"]#["OnCRPO", "CUP", "FOCOPS", "PCPO", "PPOEarlyTerminated", "PPOLag"]
     baseline_algorithms = ["PPOLag", "FOCOPS", "CUP", "PPOEarlyTerminated", "PPO", "CPO"]
-    curr_algorithms = ["PPOLag"]#, "FOCOPS", "CUP", "PPOEarlyTerminated"]
-    folder_base = "isc_lag_init"
+    curr_algorithms = ["PPOEarlyTerminated"]#["PPOLag", "FOCOPS", "CUP", "PPOEarlyTerminated"]
+    folder_base = "incremental_static_curriculum_again"
     curr_changes = [10, 20, 40, 100, 300, 700]
-    seeds = [5905, 7337, 572, 5689, 3968] # [175, 4678, 9733, 3743, 7596] # [int(rand.random() * 10000) for i in range(repetitions)]
+    seeds = [7596, 3743, 9733, 4678, 175, 3968, 5689, 572, 7337, 5905] # [5905, 7337, 572, 5689, 3968] # [175, 4678, 9733, 3743, 7596] # [int(rand.random() * 10000) for i in range(repetitions)]
     betas = [0.5, 1.0, 1.5]
     kappas = [5, 10, 20]
 
@@ -207,7 +207,7 @@ if __name__ == '__main__':
     # Repeat experiments
     wandb.login(key="4735a1d1ff8a58959d482ab9dd8f4a3396e2aa0e")
     os.environ["WANDB__SERVICE_WAIT"] = "300"
-    for end_task in range(0, len(curr_changes) + 1):
+    for end_task in range(6, len(curr_changes) + 1):
         with Pool(8) as p:
             args_base = list(product(baseline_algorithms, [end_task], ["baseline"], seeds, [1.0], [10]))
             args_curr = list(product(curr_algorithms, [end_task], ["curriculum"], seeds, [1.0], [10]))
@@ -251,14 +251,14 @@ if __name__ == '__main__':
     # for end_task in range(4, 5):
     #     use_params(*("PPOLag", end_task, "curriculum", 1142, 0.5, 10))
 
-    # Plot the results
-    # train_df = plot_train(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, include_weak=False)
+    # # Plot the results
+    # train_df = plot_adapt_tune_train(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, include_weak=False)
     # train_df.to_csv(f"./figures/{folder_base}/comparison/train_df.csv")
-    # eval_df = plot_eval(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit)
+    # eval_df = plot_adapt_tune_eval(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit)
     # eval_df.to_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
     # train_df = pd.read_csv(f"./figures/{folder_base}/comparison/train_df.csv")
     # eval_df = pd.read_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
-    # print_incremental_eval(folder=folder_base, train_df=train_df, eval_df=eval_df, save_freq=save_freq, cost_limit=cost_limit)
+    # print_adapt_tune_eval(folder=folder_base, train_df=train_df, eval_df=eval_df, save_freq=save_freq, cost_limit=cost_limit)
 
     # # for i in range(7):
     # #     use_params(*("PPOLag", i, "baseline", 42))
