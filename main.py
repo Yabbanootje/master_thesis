369f8470e30bcd544b503c344465f75a1ae659c5
Yabbanootje <k.a.b.toonen@student.tue.nl>
2024-09-03 15:26:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -293,6 +293,7 @@ if __name__ == '__main__':
                 p.starmap(use_params, args_curr + args_adapt_curr)
     elif exp == 5:
         folder_base = "incremental_adaptive_curriculum"
+        baseline_algorithms = ["PPOLag", "CUP", "PPO"]
         # Repeat experiments
         wandb.login(key="4735a1d1ff8a58959d482ab9dd8f4a3396e2aa0e")
         os.environ["WANDB__SERVICE_WAIT"] = "300"
@@ -302,6 +303,27 @@ if __name__ == '__main__':
             p.starmap(use_params, args_base)
     elif exp == 6:
         folder_base = "incremental_adaptive_curriculum"
+        baseline_algorithms = ["PPOLag", "CUP", "PPO"]
+        # Repeat experiments
+        wandb.login(key="4735a1d1ff8a58959d482ab9dd8f4a3396e2aa0e")
+        os.environ["WANDB__SERVICE_WAIT"] = "300"
+        seeds = [175, 4678, 9733, 3743, 7596]
+        with Pool(8) as p:
+            args_base = list(product(baseline_algorithms, [5], ["baseline"], seeds, [exp], [1.0], [20]))
+            p.starmap(use_params, args_base)
+    elif exp == 7:
+        folder_base = "incremental_adaptive_curriculum"
+        baseline_algorithms = ["PPOEarlyTerminated", "FOCOPS", "CPO"]
+        # Repeat experiments
+        wandb.login(key="4735a1d1ff8a58959d482ab9dd8f4a3396e2aa0e")
+        os.environ["WANDB__SERVICE_WAIT"] = "300"
+        seeds = [5905, 7337, 572, 5689, 3968]
+        with Pool(8) as p:
+            args_base = list(product(baseline_algorithms, [5], ["baseline"], seeds, [exp], [1.0], [20]))
+            p.starmap(use_params, args_base)
+    elif exp == 8:
+        folder_base = "incremental_adaptive_curriculum"
+        baseline_algorithms = ["PPOEarlyTerminated", "FOCOPS", "CPO"]
         # Repeat experiments
         wandb.login(key="4735a1d1ff8a58959d482ab9dd8f4a3396e2aa0e")
         os.environ["WANDB__SERVICE_WAIT"] = "300"
