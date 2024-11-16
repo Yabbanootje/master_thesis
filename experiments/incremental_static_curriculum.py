665ced118208756cdf24ac9a538e4813ec2b3ad6
Kelvin <kelvintoonen2001@outlook.com>
2024-11-16 14:33:58 +0100

    Clean up isc

--- experiments/incremental_static_curriculum.py
+++ experiments/incremental_static_curriculum.py
@@ -1,6 +1,15 @@
+# This file corresponds to the experiments performed in section 6.3.2 up to and including
+# section 6.3.5 of the thesis
+# Note that the results from the thesis cannot be replicated exactly with this file,
+# as throughout the different experiments here, improvements to the curriculum (such as 
+# the learning rate reset) have been made and there is no way to opt-out of these 
+# improvements
+
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from plotting.plot_functions_incremental import plot_incremental_train, plot_incremental_eval, print_incremental_eval
 from main import *
+import pandas as pd
 
 if __name__ == '__main__':
     eval_episodes = 5
@@ -16,74 +25,54 @@ if __name__ == '__main__':
     curr_changes = [10, 20, 40, 100, 300, 700]
     seeds = [int(rand.random() * 10000) for i in range(repetitions)]
 
-    # # Repeat experiments
-    # wandb.login(key="4735a1d1ff8a58959d482ab9dd8f4a3396e2aa0e")
-    # for end_task in range(1, len(curr_changes) + 1):
-    #     with Pool(8) as p:
-    #         args_base = list(product(baseline_algorithms, [end_task], ["baseline"], seeds))
-    #         args_curr = list(product(curr_algorithms, [end_task], ["curriculum"], seeds))
-    #         args = args_curr + args_base
-    #         p.starmap(use_params, args)
+    # Repeat experiments
+    wandb.login(key="4735a1d1ff8a58959d482ab9dd8f4a3396e2aa0e")
+    for end_task in range(1, len(curr_changes) + 1):
+        with Pool(8) as p:
+            args_base = list(product(baseline_algorithms, [end_task], ["baseline"], seeds, [end_task]))
+            args_curr = list(product(curr_algorithms, [end_task], ["curriculum"], seeds, [end_task]))
+            args = args_curr + args_base
+            p.starmap(use_params, args)
 
-    train_df = pd.read_csv(f"./figures/{folder_base}/comparison/train_df.csv")
-    train_df['end_task'] = train_df['end_task'].astype(str)
-    train_df['seed'] = train_df['seed'].astype(str)
-    eval_df = pd.read_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
-    eval_df['end_task'] = eval_df['end_task'].astype(str)
-    eval_df['seed'] = eval_df['seed'].astype(str)
 
-    # print(eval_df[(eval_df["end_task"] == "1") & (eval_df["type"] == "baseline")])
+    # Plot the results
+    train_df = plot_incremental_train(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit)
+    eval_df = plot_incremental_eval(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit)
+    print_incremental_eval(folder=folder_base, train_df=train_df, eval_df=eval_df, save_freq=save_freq, cost_limit=cost_limit)
+    
+    
+    # Get figures for ablation study from section 6.3.3
+    folder_base = "incremental_static_curriculum_r"
 
     # Plot the results
     train_df = plot_incremental_train(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, combined_df=train_df)
     eval_df = plot_incremental_eval(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, combined_df=eval_df)
-    # print_incremental_eval(folder=folder_base, train_df=train_df, eval_df=eval_df, save_freq=save_freq, cost_limit=cost_limit)
 
-    # train_df.to_csv(f"./figures/{folder_base}/comparison/train_df.csv")
-    # eval_df.to_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
-
-    # train_df = pd.read_csv(f"./figures/{folder_base}/comparison/train_df.csv")
-    # eval_df = pd.read_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
+    # Save results
+    train_df.to_csv(f"./figures/{folder_base}/comparison/train_df.csv")
+    eval_df.to_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
 
 
+    # Get figures for ablation study from section 6.3.4
     folder_base = "incremental_static_curriculum_ablation"
 
-    train_df = pd.read_csv(f"./figures/{folder_base}/comparison/train_df.csv")
-    train_df['end_task'] = train_df['end_task'].astype(str)
-    train_df['seed'] = train_df['seed'].astype(str)
-    eval_df = pd.read_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
-    eval_df['end_task'] = eval_df['end_task'].astype(str)
-    eval_df['seed'] = eval_df['seed'].astype(str)
-
     # Plot the results
-    # train_df = plot_incremental_train(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, combined_df=train_df)
-    # eval_df = plot_incremental_eval(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, combined_df=eval_df)
+    train_df = plot_incremental_train(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit)
+    eval_df = plot_incremental_eval(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit)
+
+    # Save results
+    train_df.to_csv(f"./figures/{folder_base}/comparison/train_df.csv")
+    eval_df.to_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
 
 
+    # Get figures for ablation study from section 6.3.5
     folder_base = "incremental_static_curriculum_lag_init"
 
-    train_df = pd.read_csv(f"./figures/{folder_base}/comparison/train_df.csv")
-    train_df['end_task'] = train_df['end_task'].astype(str)
-    train_df['seed'] = train_df['seed'].astype(str)
-    eval_df = pd.read_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
-    eval_df['end_task'] = eval_df['end_task'].astype(str)
-    eval_df['seed'] = eval_df['seed'].astype(str)
-
     # Plot the results
-    # train_df = plot_incremental_train(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, combined_df=train_df)
-    # eval_df = plot_incremental_eval(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, combined_df=eval_df)
-    
-    
-    folder_base = "incremental_static_curriculum_r"
+    train_df = plot_incremental_train(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, combined_df=train_df)
+    eval_df = plot_incremental_eval(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, combined_df=eval_df)
 
-    train_df = pd.read_csv(f"./figures/{folder_base}/comparison/train_df.csv")
-    train_df['end_task'] = train_df['end_task'].astype(str)
-    train_df['seed'] = train_df['seed'].astype(str)
-    eval_df = pd.read_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
-    eval_df['end_task'] = eval_df['end_task'].astype(str)
-    eval_df['seed'] = eval_df['seed'].astype(str)
-
-    # Plot the results
-    # train_df = plot_incremental_train(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, combined_df=train_df)
-    # eval_df = plot_incremental_eval(folder=folder_base, curr_changes=curr_changes, cost_limit=cost_limit, combined_df=eval_df)
+    # Save results
+    train_df.to_csv(f"./figures/{folder_base}/comparison/train_df.csv")
+    eval_df.to_csv(f"./figures/{folder_base}/comparison/eval_df.csv")
     
